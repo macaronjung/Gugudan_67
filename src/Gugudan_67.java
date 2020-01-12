169405373ace6b391a039c0d74315b0028d670d2
Java <Java@DESKTOP-04KTJS7>
2020-01-12 21:47:29 +0900

    initial commit

---
+++ src/Gugudan_67.java
@@ -0,0 +1,19 @@
+
+public class Gugudan_67 {
+
+	public static void main(String[] args) {
+
+		//6dan
+		int i = 1;
+		while(i<10) {
+			System.out.println(6 * i);
+			i = i + 1;
+		}
+		
+		//7dan
+		for(int j=1; j<10; j++) {
+			System.out.println(7 * j);
+		}
+	}
+
+}
